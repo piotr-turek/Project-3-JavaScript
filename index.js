6536c3a05992571c9107cab1ed80fae633cf34c7
Piotrek PC <piotrekturek286@gmail.com>
2023-04-15 19:15:16 +0200

    changed main menu, added stack icons and js file

---
+++ index.js
@@ -0,0 +1,39 @@
+const projectsList = document.querySelector("#projects-carusell ul");
+const projectItems = document.querySelectorAll("#projects-carusell ul li");
+const arrowLeft = document.querySelector("#projects-arrow--left");
+const arrowRight = document.querySelector("#projects-arrow--right");
+let itemWidth = projectItems[0].offsetWidth; // szerokość pojedynczego elementu
+
+window.onresize = function () {
+  itemWidth = projectItems[0].offsetWidth;
+};
+
+let currentPosition = 0;
+
+arrowLeft.addEventListener("click", () => {
+  if (currentPosition <= 0) {
+    // przesuwamy na koniec
+    currentPosition = projectsList.scrollWidth - projectsList.offsetWidth;
+  } else {
+    // przesuwamy o szerokość pojedynczego elementu
+    currentPosition -= itemWidth;
+  }
+  projectsList.scrollTo({
+    left: currentPosition,
+    behavior: "smooth",
+  });
+});
+
+arrowRight.addEventListener("click", () => {
+  if (currentPosition >= projectsList.scrollWidth - projectsList.offsetWidth) {
+    // przesuwamy na początek
+    currentPosition = 0;
+  } else {
+    // przesuwamy o szerokość pojedynczego elementu
+    currentPosition += itemWidth;
+  }
+  projectsList.scrollTo({
+    left: currentPosition,
+    behavior: "smooth",
+  });
+});
